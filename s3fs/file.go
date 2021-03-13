c4284fe8a95bb4bad700188ce76f5521f041b59b
Andrea <a.g.baldan@gmail.com>
2021-03-13 17:59:28 +0000

    Add some documentation to methods

--- s3fs/file.go
+++ s3fs/file.go
@@ -1,6 +1,7 @@
 package s3fs
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -12,16 +13,20 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+// S3FileInfo implements os.FileInfo interface within the S3 context
 type S3FileInfo struct {
 	key            string
 	s3ObjectOutput *s3.GetObjectOutput
 	s3Object       *s3.Object
 }
 
+// Name returns the name of the file, represented by the basename of the key
+// used to store the file into the S3 bucket
 func (i *S3FileInfo) Name() string {
 	return filepath.Base(i.key)
 }
 
+// Size returns the size of the file stored inside the S3 bucket
 func (i *S3FileInfo) Size() int64 {
 	if i.s3ObjectOutput != nil {
 		return aws.Int64Value(i.s3ObjectOutput.ContentLength)
@@ -32,13 +37,18 @@ func (i *S3FileInfo) Size() int64 {
 	return 0
 }
 
+// Mode return the file permissions of the file, given that S3 doesn't really
+// support an OS-like permission system for its content, this is limited to
+// just separating directories and files
 func (i *S3FileInfo) Mode() os.FileMode {
 	if i.IsDir() {
 		return os.ModeDir
 	}
-	return os.ModeIrregular
+	return os.ModePerm
 }
 
+// ModTime returns the LastModified time of the S3 file if present, otherwise
+// it fallbacks to the time.Time zero value
 func (i *S3FileInfo) ModTime() time.Time {
 	if i.s3ObjectOutput != nil {
 		return aws.TimeValue(i.s3ObjectOutput.LastModified)
@@ -49,10 +59,14 @@ func (i *S3FileInfo) ModTime() time.Time {
 	return time.Time{}
 }
 
+// IsDir returns true if no s3Object and s3ObjectOutput is set, in other words
+// if the current S3FileInfo is represented by only the S3 key
 func (i *S3FileInfo) IsDir() bool {
 	return i.s3ObjectOutput == nil && i.s3Object == nil
 }
 
+// Sys return the underlying data source, represented by either an
+// *s3.GetObjectOutput or an *s3.GetObject
 func (i *S3FileInfo) Sys() interface{} {
 	if i.s3ObjectOutput != nil {
 		return i.s3ObjectOutput
@@ -60,19 +74,17 @@ func (i *S3FileInfo) Sys() interface{} {
 	return i.s3Object
 }
 
-type s3lister interface {
-	ListObjectsV2(*s3.ListObjectsV2Input) (*s3.ListObjectsV2Output, error)
-}
-
 // S3File implements afero.File
 type S3File struct {
 	m              sync.RWMutex
-	s3Api          s3lister
+	s3Api          s3api
 	bucket         string
 	key            string
 	s3ObjectOutput *s3.GetObjectOutput
 }
 
+// Close closes the underlying io.ReadCloser inside the *s3.GetObjectOutput,
+// if present. Can return an error in case of already closed stream.
 func (f *S3File) Close() error {
 	if f.s3ObjectOutput != nil {
 		return f.s3ObjectOutput.Body.Close()
@@ -80,6 +92,8 @@ func (f *S3File) Close() error {
 	return nil
 }
 
+// Read read contents from the underlying *s3.GetObjectOutput into a byte
+// array, may return an error if no io.Reader is present.
 func (f *S3File) Read(p []byte) (n int, err error) {
 	if f.s3ObjectOutput == nil {
 		return 0, fmt.Errorf("Cannot read")
@@ -89,14 +103,18 @@ func (f *S3File) Read(p []byte) (n int, err error) {
 	return f.s3ObjectOutput.Body.Read(p)
 }
 
+// ReadAt unsupported
 func (f *S3File) ReadAt(p []byte, off int64) (n int, err error) {
 	return 0, nil
 }
 
+// Seek unsupported
 func (f *S3File) Seek(offset int64, whence int) (int64, error) {
 	return 0, fmt.Errorf("Not supported")
 }
 
+// Readdir returns a slice of S3FileInfo limiting the number of results based
+// on count value. Can return error if no underlying *s3.GetObjectOutput is set.
 func (f *S3File) Readdir(count int) ([]os.FileInfo, error) {
 	if f.s3ObjectOutput == nil {
 		return nil, fmt.Errorf("Cannot read directory")
@@ -109,7 +127,6 @@ func (f *S3File) Readdir(count int) ([]os.FileInfo, error) {
 	for {
 		listObjectsV2Output, err := f.s3Api.ListObjectsV2(&s3.ListObjectsV2Input{
 			Bucket:            aws.String(f.bucket),
-			Delimiter:         aws.String("/"),
 			Prefix:            aws.String(strings.TrimLeft(f.key, "/")),
 			ContinuationToken: continuationToken,
 			MaxKeys:           aws.Int64(int64(count)),
@@ -156,26 +173,39 @@ func (f *S3File) Stat() (os.FileInfo, error) {
 	}, nil
 }
 
+// Sync unsupported
 func (f *S3File) Sync() error {
 	return nil
 }
 
-// TODO
+// Write writes a slice of bytes into an S3 bucket, underlying it acts
+// differently then an OS stream, basically it overwrites the remote object
+// inside the S3 bucket by uploading the bytes over
 func (f *S3File) Write(p []byte) (n int, err error) {
-	return 0, nil
+	buf := bytes.NewBuffer(p)
+	_, err = f.s3Api.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(f.bucket),
+		Key:    aws.String(f.key),
+		Body:   aws.ReadSeekCloser(buf),
+	})
+	if err != nil {
+		return
+	}
+	n = len(p)
+	return
 }
 
-// TODO
+// WriteAt unsupported
 func (f *S3File) WriteAt(b []byte, off int64) (n int, err error) {
 	return 0, nil
 }
 
-// TODO
+// WriteString convenient way to write a string using the Write function
 func (f *S3File) WriteString(s string) (ret int, err error) {
-	return 0, nil
+	return f.Write([]byte(s))
 }
 
-// TODO
+// Truncate unsupported
 func (f *S3File) Truncate(size int64) error {
 	return nil
 }
--- s3fs/fs.go
+++ s3fs/fs.go
@@ -1,9 +1,10 @@
 package s3fs
 
 import (
+	"bytes"
 	"fmt"
-	"net/url"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -11,17 +12,32 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 )
 
+// s3api is just a convenient private interface to improve testability and
+// subsequent incremental changes.
+//
+// Could have just used s3iface.S3API but didn't feel useful to bring in such
+// a big interface while only few methods were actually useful.
+type s3api interface {
+	GetObject(*s3.GetObjectInput) (*s3.GetObjectOutput, error)
+	PutObject(*s3.PutObjectInput) (*s3.PutObjectOutput, error)
+	DeleteObject(*s3.DeleteObjectInput) (*s3.DeleteObjectOutput, error)
+	DeleteObjects(*s3.DeleteObjectsInput) (*s3.DeleteObjectsOutput, error)
+	CopyObject(*s3.CopyObjectInput) (*s3.CopyObjectOutput, error)
+	WaitUntilObjectExists(*s3.HeadObjectInput) error
+	ListObjectsV2(*s3.ListObjectsV2Input) (*s3.ListObjectsV2Output, error)
+}
+
+// S3Fs implements afero.Fs
 type S3Fs struct {
-	s3Api  s3iface.S3API
+	s3Api  s3api
 	bucket string
 }
 
-func New(bucket string, s3 s3iface.S3API) *S3Fs {
+func New(bucket string, api s3api) *S3Fs {
 	return &S3Fs{
-		s3Api:  s3,
+		s3Api:  api,
 		bucket: bucket,
 	}
 }
@@ -30,8 +46,11 @@ func (s *S3Fs) Name() string {
 	return "s3fs"
 }
 
+// Create create a new file into an S3 bucket, returning a *S3File, which
+// implements afero.File or an error
 func (s *S3Fs) Create(name string) (afero.File, error) {
 	if strings.HasSuffix(name, "/") {
+		// FIXME return err
 		return &S3File{
 			s3Api:  s.s3Api,
 			bucket: s.bucket,
@@ -39,12 +58,29 @@ func (s *S3Fs) Create(name string) (afero.File, error) {
 		}, nil
 	}
 
+	_, err := s.s3Api.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(strings.TrimLeft(name, "/")),
+		Body:   aws.ReadSeekCloser(bytes.NewBuffer([]byte{})),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return s.Open(name)
+}
+
+// Open opens a file, returning it or an error, if any happens
+func (s *S3Fs) Open(name string) (afero.File, error) {
 	getObjectOutput, err := s.s3Api.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(strings.TrimLeft(name, "/")),
 	})
 	if err != nil {
 		return nil, err
+	}
+	if aws.BoolValue(getObjectOutput.DeleteMarker) {
+		return nil, fmt.Errorf("File is marked as deleted")
 	}
 
 	return &S3File{
@@ -55,18 +91,21 @@ func (s *S3Fs) Create(name string) (afero.File, error) {
 	}, nil
 }
 
-func (s *S3Fs) Open(name string) (afero.File, error) {
-	return s.Create(name)
-}
-
+// OpenFile opens a file using the given flags, returning it or an error, if
+// any happens. Flags are unused as S3 doesn't really support OS-like
+// permissions for its contents.
 func (s *S3Fs) OpenFile(name string, flag int, perm os.FileMode) (afero.File, error) {
-	return s.Create(name)
+	return s.Open(name)
 }
 
+// Mkdir creates a directory in the filesystem, return an error if any
+// happens.
 func (s *S3Fs) Mkdir(name string, perm os.FileMode) error {
 	return s.MkdirAll(name, perm)
 }
 
+// MkdirAll creates a directory path and all parents that does not exist
+// yet.
 func (s *S3Fs) MkdirAll(name string, perm os.FileMode) error {
 	_, err := s.s3Api.PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(s.bucket),
@@ -75,23 +114,47 @@ func (s *S3Fs) MkdirAll(name string, perm os.FileMode) error {
 	return err
 }
 
+// Remove removes a file identified by name, returning an error, if any
+// happens.
 func (s *S3Fs) Remove(name string) error {
 	return s.RemoveAll(name)
 }
 
+// RemoveAll removes a directory path and any children it contains. It
+// does not fail if the path does not exist (return nil).
 func (s *S3Fs) RemoveAll(name string) error {
-	_, err := s.s3Api.DeleteObject(&s3.DeleteObjectInput{
+	listObject, err := s.s3Api.ListObjectsV2(&s3.ListObjectsV2Input{
 		Bucket: aws.String(s.bucket),
-		Key:    aws.String(name),
+		Prefix: aws.String(strings.TrimLeft(name, "/")),
+	})
+	if err != nil {
+		return err
+	}
+	if len(listObject.Contents) == 0 {
+		return fmt.Errorf("No objects")
+	}
+	fmt.Printf("#%v", listObject.Contents)
+	objectIds := make([]*s3.ObjectIdentifier, len(listObject.Contents))
+	for i, object := range listObject.Contents {
+		objectIds[i] = &s3.ObjectIdentifier{Key: object.Key}
+	}
+	_, err = s.s3Api.DeleteObjects(&s3.DeleteObjectsInput{
+		Bucket: aws.String(s.bucket),
+		Delete: &s3.Delete{
+			Objects: objectIds,
+		},
 	})
 	return err
 }
 
+// Rename renames a file. Under the hood what it does is create a copy of the
+// old file int othe s3 bucket with the new key represented by newname and then
+// remove the old copied file.
 func (s *S3Fs) Rename(oldname, newname string) error {
-	source := fmt.Sprintf("%s/%s", s.bucket, oldname)
+	source := filepath.Join(s.bucket, oldname)
 	_, err := s.s3Api.CopyObject(&s3.CopyObjectInput{
 		Bucket:     aws.String(s.bucket),
-		CopySource: aws.String(url.PathEscape(source)),
+		CopySource: aws.String(source),
 		Key:        aws.String(newname),
 	})
 	if err != nil {
@@ -111,6 +174,8 @@ func (s *S3Fs) Rename(oldname, newname string) error {
 	return err
 }
 
+// Stat returns a FileInfo describing the named file, or an error, if any
+// happens.
 func (s *S3Fs) Stat(name string) (os.FileInfo, error) {
 	getObjectOutput, err := s.s3Api.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(s.bucket),
@@ -128,14 +193,17 @@ func (s *S3Fs) Stat(name string) (os.FileInfo, error) {
 	return file.Stat()
 }
 
+// Chmod unsupported
 func (s *S3Fs) Chmod(name string, mode os.FileMode) error {
 	return nil
 }
 
+// Chown unsupported
 func (s *S3Fs) Chown(name string, mode os.FileMode) error {
 	return nil
 }
 
+// Chtimes unsupported
 func (s *S3Fs) Chtimes(name string, atime time.Time, mtime time.Time) error {
 	return nil
 }
